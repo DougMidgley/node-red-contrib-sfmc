1d9ceebecc451f2096b9566783bbf8ed13b09c46
Douglas Midgley <douglas.midgley@accenture.com>
2018-12-06 09:18:53 +0100

    Added Try Catch Blocks

--- contact-event.js
+++ contact-event.js
@@ -5,37 +5,44 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         let sfmcConfig = RED.nodes.getNode(config.sfmcConfig);
         let node = this;
+        const restEndpoint = `https://${
+            sfmcConfig.credentials.tenant
+        }.rest.marketingcloudapis.com`;
         let restClient = new SFMCRestClient({
             auth: {
                 // options you want passed when Fuel Auth is initialized
                 clientId: sfmcConfig.credentials.clientId,
                 clientSecret: sfmcConfig.credentials.clientSecret
             },
-            restEndpoint: `https://${sfmcConfig.credentials.tenant}.rest.marketingcloudapis.com`
+            restEndpoint: restEndpoint
         });
         this.on('input', function(msg) {
+            try {
+                const options = {
+                    method: 'POST',
+                    uri: restEndpoint + '/interaction/v1/events',
+                    body: msg[config.payloadKey],
+                    json: true
+                };
 
-
-            const options = {
-                method: 'POST',
-                url: 'https://www.exacttargetapis.com/interaction/v1/events',
-                body: msg[config.payloadKey],
-                json: true
-            };
-
-            restClient.post(options)
-                .then((response) => {
-                    node.send({
-                        payload: response.body.Results
-                    });
-                })
-                .catch((err) =>
-                    node.error(err, {
-                        payload: JSON.parse(JSON.stringify(err))
+                restClient
+                    .post(options)
+                    .then((response) => {
+                        node.send({
+                            payload: response.body
+                        });
                     })
-                );
-        })
-        ;
+                    .catch((err) => {
+                        node.error(err, {
+                            payload: JSON.parse(JSON.stringify(err))
+                        });
+                    });
+            } catch (ex) {
+                node.error(ex, {
+                    payload: JSON.parse(JSON.stringify(ex))
+                });
+            }
+        });
     }
     RED.nodes.registerType('contact-event', ContactEvent);
 };
